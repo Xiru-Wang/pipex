d78a7247bcb786236e9fde079c5afd280d694a89
Xiru Wang <xiwang@c4b3c1.42berlin.de>
2023-08-17 19:47:59 +0200

    update

--- pipex.c
+++ pipex.c
@@ -1,28 +1,29 @@
 #include <unistd.h>//fork pipe access
 #include <fcntl.h>//open  int open(const char *path, int oflag, ...);
+#include <stdio.h>//perror
 //https://github.com/jdecorte-be/42-Pipex/tree/master
 //https://codeberg.org/ilshat/Pipex/src/branch/main/My_pipex/pipex.c
 //pipe() takes an array of two int such as int end[2], and links them together.
 //end[1] will write to the its own fd, and end[0]
 //will read end[1]’s fd and write to its own
 
-void	pipex(int f1, int f2, char *cmd1, char *cmd2)
-{
-	int		fd[2];
-	pid_t	pid;
+// void	pipex(int f1, int f2, char *cmd1, char *cmd2)
+// {
+// 	int		fd[2];
+// 	pid_t	pid;
 
-	pipe(fd);
-	pid = fork();
-	if (pid < 0)
-	{
-		perror("fork failed");
-		return (1);
-	}
-	if (pid == 0)
-		child(f1, cmd1);
-	else
-		parent(f2, cmd2);
-}
+// 	pipe(fd);
+// 	pid = fork();
+// 	if (pid < 0)
+// 	{
+// 		perror("fork failed");
+// 		return (1);
+// 	}
+// 	if (pid == 0)
+// 		child(f1, cmd1);
+// 	else
+// 		parent(f2, cmd2);
+// }
 
 //int dup2(int fd1, int fd2) [swap]
 //it will close fd2 and duplicate the value of fd2 to fd1
@@ -54,16 +55,18 @@ int	main(int ac, char **av, char **envp)
 	return (0);
 }
 
+//int access(char *pathname. int mode)
+//F_OK existence R_OK W_OK X_OK read, write, execute
 void	child(char **av, char **envp, int *fd)
 {
 	if (access(av[1], F_OK) != 0)// if file does not exist
 	{
-		write(2, "No such file or directory error\n", 32);
+		write(2, "No such file or directory\n", 32);
 		exit(1);
 	}
 	if (access(av[1], R_OK) != 0)
 	{
-		write(2, "Permission denied error\n", 32);
+		write(2, "Permission denied\n", 32);
 		exit(1);
 	}
 	dup2(fd[1], 1); //swap stdout to fd[1] why not stdin first??
